3dce2fe76b0f164b0c80bde004d9798367811d18
holmeskone <165698602+holmeskone@users.noreply.github.com>
2025-01-01 16:29:20 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import "./style.css"; // Import CSS styles 
+import "./style.css"; // Import CSS styles
 
-console.log('hello world')
+console.log("hello world");
 
-const content = document.getElementById('content');
-const tester = document.createElement('p');
-tester.textContent = 'Hello, this is a test';
+const content = document.getElementById("content");
+const tester = document.createElement("p");
+tester.textContent = "Hello, this is a test";
 content.appendChild(tester);
