fe0dc98ad91c4f3b1b66daf3ca2778f26c09fd70
holmeskone <165698602+holmeskone@users.noreply.github.com>
2025-01-02 21:27:33 +0100

    Import styling and listen for search click and call search function

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
+import "./style.css";
+import { searchCity } from "./js/components/searchBar";
 console.log("hello world");
+const clickInput = document.getElementById("search");
 
-async function getWeather(city) {
-  console.log(city);
-  //   const city = document.getElementById("city");
-  //   let cityValue = city.value;
-  const response = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=WVR3K9GYJ4ZQJJRHS6AFF8E2K`,
-    { mode: "cors" }
-  );
-  const weatherData = await response.json();
-  const weatherCity = weatherData.resolvedAddress;
-  const weatherDescription = weatherData.description;
-  console.log(weatherCity, weatherDescription);
-}
+clickInput.addEventListener("click", () => {
+  searchCity();
+});
