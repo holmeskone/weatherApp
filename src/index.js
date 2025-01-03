846b6c40a878ddcf527a34e6ac5f9d4dc29c9d6e
holmeskone <165698602+holmeskone@users.noreply.github.com>
2025-01-03 11:31:47 +0100

    Import toggle functions and call them on load and when toogle button is clicked, send the unit saved in local storage as to the toggle function, default unit is the metric system

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import "./style.css";
 import { searchCity } from "./js/components/searchBar";
-console.log("hello world");
-const clickInput = document.getElementById("search");
+import { toogleDefaultMetrics, toogleMetrics } from "./js/components/toggle";
 
+document.addEventListener("DOMContentLoaded", () => {
+  toogleDefaultMetrics();
+  console.log(localStorage.getItem("unit"));
+});
+console.log(localStorage.getItem("unit"));
+const metricToggle = document.querySelector("[data-metric-toggle]");
+metricToggle.addEventListener("click", () => {
+  toogleMetrics(localStorage.getItem("unit"));
+  console.log(localStorage.getItem("unit"));
+});
+
+const clickInput = document.getElementById("search");
 clickInput.addEventListener("click", () => {
-  searchCity();
+  searchCity(localStorage.getItem("unit"));
 });
