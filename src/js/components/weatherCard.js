23c4d5a64b30b9569a6b75452447e5b43fdfbcf3
holmeskone <165698602+holmeskone@users.noreply.github.com>
2025-01-03 15:52:31 +0100

    Create a function that creates the div's for the weather card that includes the current weather, small description, max temperature and min temperature

---
+++ src/js/components/weatherCard.js
@@ -0,0 +1,42 @@
+export function weatherCard(
+  city,
+  currentTemperature,
+  conditions,
+  maxTemperature,
+  minTemperature
+) {
+  const weatherCardDiv = document.createElement("div");
+  weatherCardDiv.id = "weather-card";
+  const weatherCardTemperature = document.createElement("div");
+  weatherCardTemperature.id = "weather-temperature";
+  const weatherMaxMin = document.createElement("div");
+  weatherMaxMin.id = "weather-maxmin";
+  const weatherCardDescription = document.createElement("div");
+  weatherCardDescription.id = "weather-description";
+  const temperature = document.createElement("h3");
+  const currentDescription = document.createElement("h3");
+  const maxTemp = document.createElement("h3");
+  const minTemp = document.createElement("h3");
+  temperature.innerHTML = currentTemperature;
+  currentDescription.innerHTML = conditions;
+  maxTemp.innerHTML = maxTemperature;
+  minTemp.innerHTML = minTemperature;
+
+  weatherCardTemperature.appendChild(temperature);
+  weatherCardDescription.appendChild(currentDescription);
+  weatherMaxMin.append(maxTemp, minTemp);
+  weatherCardDiv.append(
+    weatherCardTemperature,
+    weatherCardDescription,
+    weatherMaxMin
+  );
+  document.body.appendChild(weatherCardDiv);
+
+  console.log(
+    city,
+    currentTemperature,
+    conditions,
+    maxTemperature,
+    minTemperature
+  );
+}
