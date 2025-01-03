b42ad4e940eaa8b3afeddba6fac893cbb901477a
holmeskone <165698602+holmeskone@users.noreply.github.com>
2025-01-03 16:44:51 +0100

    Create a div that will contain today's description and today's hours, next step will be to display temperature. We iterate through each hour and append to the div

---
+++ src/js/components/hourlyCarousel.js
@@ -0,0 +1,20 @@
+export function hourlyCarousel(description, hours) {
+  const hourlyCarouselSection = document.createElement("div");
+  hourlyCarouselSection.id = "hour-carousel";
+  const todayDescription = document.createElement("div");
+  todayDescription.id = "today-description-carousel";
+  const descriptionContent = document.createElement("p");
+  descriptionContent.innerText = description;
+  todayDescription.append(descriptionContent);
+
+  const hourlySection = document.createElement("div");
+  hourlySection.id = "hourly-section";
+  hours.forEach((hour) => {
+    console.log(hour.datetime);
+    const hourOfDay = document.createElement("p");
+    hourOfDay.innerHTML = hour.datetime;
+    hourlySection.append(hourOfDay);
+  });
+  hourlyCarouselSection.append(hourlySection);
+  document.body.append(hourlyCarouselSection, todayDescription);
+}
