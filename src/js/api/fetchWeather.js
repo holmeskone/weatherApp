cdefa9e27ff2ac96ff458ec48a5c87d9d8893b08
holmeskone <165698602+holmeskone@users.noreply.github.com>
2025-01-03 09:59:15 +0100

    Add error handling statements and reroute to error handling function

--- src/js/api/fetchWeather.js
+++ src/js/api/fetchWeather.js
@@ -1,13 +1,17 @@
+import { errorHandling } from "./errorHandling";
 export async function getWeather(city) {
-  console.log(city);
-  //   const city = document.getElementById("city");
-  //   let cityValue = city.value;
-  const response = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=WVR3K9GYJ4ZQJJRHS6AFF8E2K`,
-    { mode: "cors" }
-  );
-  const weatherData = await response.json();
-  const weatherCity = weatherData.resolvedAddress;
-  const weatherDescription = weatherData.description;
-  console.log(weatherCity, weatherDescription);
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=WVR3K9GYJ4ZQJJRHS6AFF8E2K`,
+      { mode: "cors" }
+    );
+    const weatherData = await response.json();
+    const weatherCity = weatherData.resolvedAddress;
+    const weatherDescription = weatherData.description;
+    const weatherDays = weatherData.days;
+    console.log(weatherData, weatherCity, weatherDescription, weatherDays);
+  } catch (err) {
+    console.log(err);
+    errorHandling(city);
+  }
 }
