6b4cc65dc7ba253168add3b063d478b28cb34f22
holmeskone <165698602+holmeskone@users.noreply.github.com>
2025-01-03 16:46:18 +0100

    Create more constants that are parts of the API response, and use them as variables for functions like display location, or the hourlyCarousel, that will display the weather by the hour of the day

--- src/js/api/fetchWeather.js
+++ src/js/api/fetchWeather.js
@@ -1,4 +1,7 @@
 import { errorHandling } from "./errorHandling";
+import { displayLocation } from "../components/location";
+import { weatherCard } from "../components/weatherCard";
+import { hourlyCarousel } from "../components/hourlyCarousel";
 export async function getWeather(city, unit) {
   try {
     const response = await fetch(
@@ -7,9 +10,22 @@ export async function getWeather(city, unit) {
     );
     const weatherData = await response.json();
     const weatherCity = weatherData.resolvedAddress;
-    const weatherDescription = weatherData.description;
-    const weatherDays = weatherData.days;
-    console.log(weatherData, weatherCity, weatherDescription, weatherDays);
+    const weatherTemperature = weatherData.days[0].temp;
+    const weatherConditions = weatherData.days[0].conditions;
+    const weatherMaxTemp = weatherData.days[0].tempmax;
+    const weatherMinTemp = weatherData.days[0].tempmin;
+    const weatherDescription = weatherData.days[0].description;
+    const weatherHours = weatherData.days[0].hours;
+    console.log(weatherData);
+    displayLocation(weatherCity);
+    weatherCard(
+      weatherCity,
+      weatherTemperature,
+      weatherConditions,
+      weatherMaxTemp,
+      weatherMinTemp
+    );
+    hourlyCarousel(weatherDescription, weatherHours);
   } catch (err) {
     console.log(err);
     errorHandling(city);
