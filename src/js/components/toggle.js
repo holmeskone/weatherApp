e373452c9733c01d343b2c8da93797c02f500adc
holmeskone <165698602+holmeskone@users.noreply.github.com>
2025-01-03 14:47:33 +0100

    Add feature to change text of button depending on their current unit, if unit is celcius the button displays Change to Fahrenheit, and viceversa

--- src/js/components/toggle.js
+++ src/js/components/toggle.js
@@ -1,18 +1,29 @@
+let toggleButton = document.querySelector("[data-metric-toggle]");
 export function toogleDefaultMetrics() {
   if (localStorage.getItem("unit") === null) {
     localStorage.setItem("unit", "metric");
     console.log(
-      `Has automatically been alocated a metric ${localStorage.getItem("unit")}`
+      `Has automatically been alocated a unit ${localStorage.getItem("unit")}`
     );
+    toggleButton.innerHTML = "Change to fahrenheit";
+  } else {
+    if (localStorage.getItem("unit") === "metric") {
+      toggleButton.innerHTML = "Change to fahrenheit";
+    } else {
+      toggleButton.innerHTML = "Change to celcius";
+    }
   }
 }
 
 export function toogleMetrics(unit) {
   if (unit === "us") {
+    toggleButton.innerHTML = "";
     localStorage.setItem("unit", "metric");
     console.log(`It was fahrenheit, now it's celcius`);
+    toggleButton.textContent = "Change to fahrenheit";
   } else {
     localStorage.setItem("unit", "us");
     console.log(`It was celcius, now it's fahrenheit`);
+    toggleButton.innerHTML = "Change to celcius";
   }
 }
