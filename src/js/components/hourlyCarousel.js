99bab15bd1995f07de0f780fc174df597092bd6a
holmeskone <165698602+holmeskone@users.noreply.github.com>
2025-01-04 19:31:16 +0100

    Made ability to display temperature and icon in hourly carousel section

--- src/js/components/hourlyCarousel.js
+++ src/js/components/hourlyCarousel.js
@@ -1,20 +1,34 @@
 export function hourlyCarousel(description, hours) {
+  // Create elements to display API reponse information
   const hourlyCarouselSection = document.createElement("div");
-  hourlyCarouselSection.id = "hour-carousel";
   const todayDescription = document.createElement("div");
-  todayDescription.id = "today-description-carousel";
   const descriptionContent = document.createElement("p");
+  const hourlySection = document.createElement("div");
+  // Add IDs to all created elements
+  hourlyCarouselSection.id = "hour-carousel";
+  todayDescription.id = "today-description-carousel";
+  hourlySection.id = "hourly-section";
+  //Add API description as innerText of descrption area
   descriptionContent.innerText = description;
   todayDescription.append(descriptionContent);
 
-  const hourlySection = document.createElement("div");
+  //Iterate over the hours object and return each hour, icon and temperature for the day.
   hourlySection.id = "hourly-section";
   hours.forEach((hour) => {
-    console.log(hour.datetime);
+    console.log(hour.datetime, hour.icon, hour.temp);
     const hourOfDay = document.createElement("p");
+    const iconOfHour = document.createElement("i");
+    const temperatureOfHour = document.createElement("p");
     hourOfDay.innerHTML = hour.datetime;
-    hourlySection.append(hourOfDay);
+    iconOfHour.className = "fa-solid fa-cloud-moon";
+    temperatureOfHour.innerHTML = hour.temp;
+    hourlySection.append(hourOfDay, iconOfHour, temperatureOfHour);
   });
+
+  //Dependant on the icon in API, call one FontAwesome icon or another
+
+  //   function iconIdentifier(icon){
+  //   }
   hourlyCarouselSection.append(todayDescription, hourlySection);
   document.body.append(hourlyCarouselSection);
 }
