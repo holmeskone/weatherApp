60a1a2874f4c74468ef36dd876d49b55a6b83a38
holmeskone <165698602+holmeskone@users.noreply.github.com>
2025-01-06 10:41:37 +0100

    Import dayCarousel and send API variables that correspond to the fornightly weather forecast

--- src/js/api/fetchWeather.js
+++ src/js/api/fetchWeather.js
@@ -3,6 +3,7 @@ import { displayLocation } from "../components/location";
 import { weatherCard } from "../components/weatherCard";
 import { hourlyCarousel } from "../components/hourlyCarousel";
 import { weatherStats } from "../components/weatherStats";
+import { dayCarousel } from "../components/dayCarousel";
 export async function getWeather(city, unit) {
   try {
     const response = await fetch(
@@ -10,19 +11,24 @@ export async function getWeather(city, unit) {
       { mode: "cors" }
     );
     const weatherData = await response.json();
+    //constants for the weatherCard Section
     const weatherCity = weatherData.resolvedAddress;
     const weatherTemperature = weatherData.days[0].temp;
     const weatherConditions = weatherData.days[0].conditions;
     const weatherMaxTemp = weatherData.days[0].tempmax;
     const weatherMinTemp = weatherData.days[0].tempmin;
+    //Constants for the hourlyCarousel
     const weatherDescription = weatherData.days[0].description;
     const weatherHours = weatherData.days[0].hours;
+    //Constants for the weatherStats
     const weatherFeelsLike = weatherData.days[0].feelslike;
     const weatherHumidity = weatherData.days[0].humidity;
     const weatherWind = weatherData.days[0].windspeed;
     const weatherUV = weatherData.days[0].uvindex;
     const weatherSunrise = weatherData.days[0].sunrise;
     const weatherSunset = weatherData.days[0].sunset;
+    //constants for the dayCarousel
+    const dailyWeather = weatherData.days;
     console.log(weatherData);
     displayLocation(weatherCity);
     weatherCard(
@@ -41,6 +47,7 @@ export async function getWeather(city, unit) {
       weatherSunrise,
       weatherSunset
     );
+    dayCarousel(dailyWeather);
   } catch (err) {
     console.log(err);
     errorHandling(city);
