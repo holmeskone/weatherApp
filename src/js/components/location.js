5c1782d3a7873b7882ef5b147241fb3a2cd502ee
holmeskone <165698602+holmeskone@users.noreply.github.com>
2025-01-06 18:27:21 +0100

    Split city variable to only display city and country

--- src/js/components/location.js
+++ src/js/components/location.js
@@ -2,7 +2,11 @@ export function displayLocation(city) {
   const cityElement = document.createElement("div");
   cityElement.id = "location";
   const cityValue = document.createElement("h2");
-  cityValue.innerHTML = city;
-  cityElement.appendChild(cityValue);
+  const countryValue = document.createElement("h3");
+  // Split the string by commas and trim any extra spaces
+  const parts = city.split(",").map((part) => part.trim());
+  cityValue.innerHTML = parts[0];
+  countryValue.innerHTML = parts[parts.length - 1];
+  cityElement.append(cityValue, countryValue);
   document.body.appendChild(cityElement);
 }
