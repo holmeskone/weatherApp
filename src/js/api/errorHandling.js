60d69b5205654f559600c12b999aa1b31b5e3346
holmeskone <165698602+holmeskone@users.noreply.github.com>
2025-01-09 14:19:46 +0100

    Add error message when city doesn't exist

--- src/js/api/errorHandling.js
+++ src/js/api/errorHandling.js
@@ -1,3 +1,18 @@
 export function errorHandling(city) {
   console.log(`Sorry, ${city} was not found in out database`);
+  // Create the outer div for the loading spinner
+  const weatherSection = document.getElementById("weather-section");
+  // Create the spinner element
+  const error = document.createElement("div");
+  error.className = "error";
+
+  // Create the loading text
+  const errorText = document.createElement("p");
+  errorText.textContent = "The city was not found, please try again. ";
+
+  // Append the spinner and text to the loadingSpinner div
+  error.appendChild(errorText);
+
+  // Append the loadingSpinner to the body (or any other container)
+  weatherSection.append(error);
 }
